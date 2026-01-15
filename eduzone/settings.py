acfdfbb9725a10f05772223b54b3c6e8c3a13f00
Sudhanshu <chaudharysudhanshu385@gmail.com>
2026-01-15 19:47:04 +0530

    add whitenoise

--- eduzone/settings.py
+++ eduzone/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -135,3 +135,4 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 CKEDITOR_UPLOAD_PATH = "uploads/"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressManifestStaticFileStorage'
