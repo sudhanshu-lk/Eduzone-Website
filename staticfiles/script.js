b0ac07c5157eedda1aa916b30d8bd677cf815b21
Sudhanshu <chaudharysudhanshu385@gmail.com>
2026-01-16 18:31:26 +0530

    static files for vercel

---
+++ staticfiles/script.js
@@ -0,0 +1,21 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     const navbarContainer = document.getElementById('navbar-container');
+//     fetch('navbar.html')
+//         .then(data => Response.text())
+//         .then(data => {
+//             document.getElementById('navbar-placeholder').innerHTML = data;
+//         });
+// });
+
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     const navLinks = document.querySelectorAll('.nav-link');
+//     const currentPath = window.location.pathname;
+//     navLinks.forEach(link => {
+//         const linkPath = link.getAttribute('href');
+//         if (currentPath.includes(linkPath)) {
+//             navLinks.forEach(item => item.classList.remove('active'));
+//             link.classList.add('active');
+//         }
+//     });
+// });
